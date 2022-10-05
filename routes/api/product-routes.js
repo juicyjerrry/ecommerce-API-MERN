ce4d1346580c174a39e83bc7378908ab982514c2
Jeremiah Kendl <kendljeremiah@gmail.com>
2022-10-04 19:59:17 -0500

    StartingProduct routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag
+      }
+    ]
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
