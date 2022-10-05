1041ee44000b713e4e45e10e4c4f1c1b678ac0ba
Jeremiah Kendl <kendljeremiah@gmail.com>
2022-10-04 19:30:23 -0500

    Tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,7 +37,7 @@ router.get('/:id', (req, res) => {
   })
     .then(dbTagData => {
       if (!dbTagData) {
-        res.status(404).json({ message: 'No tag id' });
+        res.status(404).json({ message: "No tag with this id" });
         return;
       }
       res.json(dbTagData);
@@ -64,7 +64,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
-  
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No tag with this id" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
