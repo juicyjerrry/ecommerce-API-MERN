c95f5ce74df242591e61e7f98fbfc08af96bec51
Jeremiah Kendl <kendljeremiah@gmail.com>
2022-10-04 19:34:41 -0500

    Update tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -60,6 +60,24 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body,
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No tag with this id" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
